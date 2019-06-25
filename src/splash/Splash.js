38419bdc9fe051a6cc98906ae6ee2a9dafc2331d
DouweCnossen <d.cnossen@metrological.com>
2019-06-25 10:59:37 -0700

    update(challenge 1): added logo

--- src/splash/Splash.js
+++ src/splash/Splash.js
@@ -3,7 +3,10 @@ import App from "../App.js";
 export default class Splash extends lng.Component{
     static _template() {
         return {
-
+            Logo: {
+                mount: .5, x: 960, y: 540, color: 0xff01d277,
+                texture: lng.Tools.getSvgTexture(App.getPath('images/tmdb-logo.svg'), 600, 500)
+            }
         }
     }
 }
